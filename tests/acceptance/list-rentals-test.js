f15797ac2303484c05a7af531993bfaccdba1371
Marc Altmann <marcaltmann@posteo.de>
2019-02-12 16:32:53 +0100

    Add tests for map functionality

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,3 +1,4 @@
+import Service from '@ember/service';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
@@ -9,10 +10,21 @@ import {
   triggerKeyEvent,
   visit,
 } from '@ember/test-helpers';
+import { resolve } from 'rsvp';
+
+let StubMapsService = Service.extend({
+  getMapElement() {
+    return resolve(document.createElement('div'));
+  }
+});
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+
+  hooks.beforeEach(function() {
+    this.owner.register('service:map-element', StubMapsService);
+  });
 
   test('should show rentals on the home page', async function (assert) {
     await visit('/');
